313c8b5d92124b57506ebf6eb3f152c80bb7f45d
Alexandre Zagame <zagamealexandre@gmail.com>
2021-01-27 14:16:11 +0100

    deploying database front and back to zeet

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import postRoutes from './routes/posts.js';
 import userRouter from './routes/user.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -15,12 +17,17 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use('/user', userRouter);
 
-const CONNECTION_URL =
-  'mongodb+srv://alexandrezagame:Bvd3az5e!azb@cluster0.4kstl.mongodb.net/<dbname>?retryWrites=true&w=majority';
+app.get('/', (req, res) => {
+  res.send('Hello to memories API');
+});
+
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() =>
     app.listen(PORT, () =>
       console.log(`Server Running on Port: http://localhost:${PORT}`)
